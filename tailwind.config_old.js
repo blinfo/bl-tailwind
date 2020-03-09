be35cd839f0a2a226ee5d1e67a9b7e3b445da3bb
William Wallin <william.wallin@blinfo.se>
2020-03-09 10:36:34 +0100

    BL Design refactoring

--- tailwind.config_old.js
+++ tailwind.config_old.js
@@ -45,51 +45,49 @@ View the full documentation at https://tailwindcss.com.
 
 let colors = {
   'transparent': 'transparent',
-  'black': '#22292f',
+  'black': '#000000',
   'white': '#ffffff',
-  'bl-background': '#F1F6F8',
+
+  'bl-grey-900': '#161e2e',
+  'bl-grey-800': '#252f3f',
+  'bl-grey-700': '#374151',
+  'bl-grey-600': '#4b5563',
+  'bl-grey-500': '#6b7280',
+  'bl-grey-400': '#9fa6b2',
+  'bl-grey-300': '#d2d6dc',
+  'bl-grey-200': '#e5e7eb',
+  'bl-grey-100': '#f7fafc',
+  'bl-grey-50': '#f9fafb',
 
   'bl-red-800': '#661715',
   'bl-red-600': '#cc2f2a',
   'bl-red-500': '#e3342f',
   'bl-red-200': '#f8cccb',
   'bl-red-100': '#fcebea',
-
+    
   'bl-green-800': '#195733',
   'bl-green-600': '#32ae67',
   'bl-green-500': '#38c172',
   'bl-green-200': '#cdf0dc',
   'bl-green-100': '#ebf9f1',
-
-  'bl-orange-800': '#6D4714',
-  'bl-orange-600': '#DB8D29',
-  'bl-orange-500': '#F39D2D',
-  'bl-orange-200': '#FCE7CB',
-  'bl-orange-100': '#FEF5EA',
-
-  'bl-blue-800': '#061A25',
-  'bl-blue-700': '#082331',
-  'bl-blue-600': '#0C344A',
-  'bl-blue-500': '#0D3A52',
-  'bl-blue-400': '#567586',
-  'bl-blue-300': '#9EB0BA',
-  'bl-blue-200': '#C3CED4',
-  'bl-blue-100': '#E7EBEE',
-
+    
+  'bl-orange-800': '#6d4714',
+  'bl-orange-600': '#dB8d29',
+  'bl-orange-500': '#f39d2d',
+  'bl-orange-200': '#fce7cb',
+  'bl-orange-100': '#fef5ea',
+    
+  'bl-blue-800': '#061a25',
+  'bl-blue-600': '#0c344a',
+  'bl-blue-500': '#0d3a52',
+  'bl-blue-200': '#c3ced4',
+  'bl-blue-100': '#e7ebee',
+    
   'bl-blue-light-800': '#284956',
-  'bl-blue-light-600': '#4F93AB',
-  'bl-blue-light-500': '#58A3BE',
-  'bl-blue-light-200': '#D5E8EF',
-  'bl-blue-light-100': '#EEF6F9',
-
-  'bl-grey-800': '#2D3748',
-  'bl-grey-700': '#4A5568',
-  'bl-grey-600': '#718096',
-  'bl-grey-500': '#A0AEC0',
-  'bl-grey-400': '#CBD5E0',
-  'bl-grey-300': '#E2E8F0',
-  'bl-grey-200': '#EDF2F7',
-  'bl-grey-100': '#F7FAFC',
+  'bl-blue-light-600': '#4f93ab',
+  'bl-blue-light-500': '#58a3be',
+  'bl-blue-light-200': '#d5e8ef',
+  'bl-blue-light-100': '#eef6f9',
 }
 
 module.exports = {
@@ -361,7 +359,7 @@ module.exports = {
   |
   */
 
-  borderColors: global.Object.assign({ default: colors['grey-light'] }, colors),
+  borderColors: global.Object.assign({ default: colors['bl-grey-200'] }, colors),
 
 
   /*
